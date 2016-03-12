8b2061e587c14fe4fa0a461dc6bf7e03cda4a8ec
menglexing <menglexing@yy.com>
2016-03-12 21:51:44 +0800

    优化

--- Processor.js
+++ Processor.js
@@ -33,26 +33,63 @@
             this.loop()
         },
         loop: function () {
-            var self = this
-
-            for (var i = 0, l = this.threads.length; i < l; i++) {
-                var thread = this.threads[i]
-
-                this.threads[i] = thread.pipe(function(i){    // then
-                    if(self.tasks.length > 0) {
-                        var defer = $.Deferred()
-                        var release = function () {
-                            defer.resolve(i)
-                        }
-
-                        self.tasks.shift()(release, i)    // 需要task内部主动释放线程: release()
-
-                        return defer.promise()
-                    } else {
-                        return i
-                    }
-                })
+            if (this.locked || this.tasks.length == 0) {
+                return
             }
+
+            var self = this
+            var index = this.indexOfIdle()
+            var threads = this.threads
+
+            if (index != -1) {
+                var task = this.tasks.shift()
+
+                threads[index] = threads[index].pipe(function(i){    // then
+                    var defer = $.Deferred()
+                    var release = function () {
+                        defer.resolve(i)
+                    }
+
+                    task(release, i)
+
+                    return defer.promise()
+                })
+            } 
+            else {
+                this.locked = true
+
+                for (var i = 0, l = threads.length; i < l; i++) {
+
+                    threads[i] = threads[i].pipe(function(i){    // then
+                        if(self.tasks.length > 0) {
+                            var defer = $.Deferred()
+                            var release = function () {
+                                defer.resolve(i)
+                                self.locked = false
+                                if (self.tasks.length > 0) self.loop()
+                            }
+
+                            self.tasks.shift()(release, i)    // 需要task内部主动释放线程: release()
+
+                            return defer.promise()
+                        } else {
+                            self.locked = false;
+                            return i
+                        }
+                    })
+
+                }
+            }
+        },
+        indexOfIdle: function () {
+            var threads = this.threads
+            var index = -1
+
+            for (var i = 0, l = threads.length; i < l; i++) {
+                if (threads[i].state() == 'resolved') return i
+            }
+
+            return index
         }
     }
 
