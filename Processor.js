2cb45d1cd3490fadbc51f735c0472404d7a438b5
Jim <307083215@qq.com>
2016-03-13 14:10:14 +0800

    bugfix

--- Processor.js
+++ Processor.js
@@ -14,7 +14,7 @@
      */
     function Processor (n) {
         n = n || 1;
-        
+
         this.tasks = []
         this.threads = []
 
@@ -25,8 +25,6 @@
 
     Processor.prototype = {
         process: function (task) {
-            var self = this;
-
             if (typeof task !== 'function') return;
 
             this.tasks.push(task)
@@ -39,49 +37,29 @@
 
             var self = this
             var index = this.indexOfIdle()
-            var threads = this.threads
 
-            if (index != -1) {
-                var task = this.tasks.shift()
-
-                threads[index] = threads[index].pipe(function(i){    // then
-                    var defer = $.Deferred()
-                    var release = function () {
-                        defer.resolve(i)
-                    }
-
-                    task(release, i)
-
-                    return defer.promise()
-                })
-            } 
-            else {
-                this.locked = true
-
-                for (var i = 0, l = threads.length; i < l; i++) {
-
-                    threads[i] = threads[i].pipe(function(i){    // then
-                        if(self.tasks.length > 0) {
-                            var defer = $.Deferred()
-                            var release = function () {
-                                setTimeout(function(){    // 防止release在task内部被同步执行
-                                    defer.resolve(i)
-                                    self.locked = false
-                                    if (self.tasks.length > 0) self.loop()
-                                }, 0)
-                            }
-
-                            self.tasks.shift()(release, i)    // 需要task内部主动释放线程: release()
-
-                            return defer.promise()
-                        } else {
-                            self.locked = false;
-                            return i
-                        }
-                    })
-
-                }
+            if (index == -1) {
+                this.locked = true;
+                return
             }
+
+            var task = this.tasks.shift()
+
+            this.threads[index] = this.threads[index].pipe(function(i){    // then
+                var defer = $.Deferred()
+                var release = function () {
+                    defer.resolve(i)
+
+                    setTimeout(function(){    // 防止release在task内部被同步时导致的不良后果
+                        self.locked = false
+                        if (self.tasks.length > 0) self.loop()
+                    }, 0)
+                }
+
+                task(release, i)
+
+                return defer.promise()
+            })
         },
         indexOfIdle: function () {
             var threads = this.threads
